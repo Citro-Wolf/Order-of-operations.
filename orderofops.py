0db542c205b1495689ced930b069de5e779e1a78
Citro-Wolf <griffinbr@bentonvillek12.org>
2024-08-28 13:37:55 -0500

    Add files via upload

---
+++ orderofops.py
@@ -0,0 +1,8 @@
+# Do not edit the X variable! X is always 1. The others you can change. 
+x = 1
+A = 2*x**2 + 5
+B = x**(1/2) - 10
+C = 5*1**5 +4*x**2 - 2
+print("A is:",A)
+print("B is:",B)
+print("C is:",C)
